a5415576ac9858e03e534c687e0b10a651fd1cd4
ChenxiYuan <yuanchenxi95@gmail.com>
2018-11-29 20:36:35 -0500

    db_rolling

--- db_rolling.py
+++ db_rolling.py
@@ -13,7 +13,7 @@ ETHER = 'Ether'
 # writeConcern = pymongo.write_concern.WriteConcern(w=0, wtimeout=None, j=None, fsync=None)
 client = MongoClient(MONGO_DB_ADDRESS, serverSelectionTimeoutMS=1)
 scapy_database = client['scapy']
-http_data_collection = scapy_database['tcpdatas']\
+http_data_collection = scapy_database['tcpdatas']
   # .with_options(write_concern=writeConcern)
 
 
@@ -22,18 +22,24 @@ def one_day_before_milli_time():
   return int(round(one_day_before * 1000))
 
 
+def one_hour_before_milli_time():
+  one_day_before = time.time() - 60 * 60
+  return int(round(one_day_before * 1000))
+
 def main():
     logging.basicConfig(filename='db_rolling.log', level=logging.INFO, format='%(asctime)s %(message)s')
     logging.info('Started')
     try:
-      time_to_be_deleted = one_day_before_milli_time()
+      time_to_be_deleted = one_hour_before_milli_time()
       result = http_data_collection.delete_many({
         'timestamp': {
           '$lt': time_to_be_deleted
         }
       })
+      deleted_count = result.deleted_count
+
       logging.info('delete entries before ' + str(time_to_be_deleted))
-      logging.info('number of records deleted: ' + str(result.deleted_count))
+      logging.info('number of records deleted: ' + deleted_count)
     except Exception as e:
       logging.error('Error: ' + str(e))
     logging.info('Finished')
